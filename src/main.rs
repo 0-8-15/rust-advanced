5c23e806fc8b249534ebf4dbd36087443fbe403a
WITTENBERGER Joerg (SAFRAN ENGINEERING SERVICES) <L045738@one.ad>
2024-01-10 11:55:16 +0100

    Debug info macro and time measurement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use debug_info;
 use std::{thread::sleep, time::Duration};
 
-#[debug_info::log("huch")]
+#[debug_info::measure_time("huch")]
 fn calc(calories_per_bite: i32, bites: i32) -> i32 {
     println!("calculating...");
     sleep(Duration::from_secs(1));
