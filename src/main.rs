36ba2762be9e5825afee9c740e6eca201c902402
WITTENBERGER Joerg (SAFRAN ENGINEERING SERVICES) <L045738@one.ad>
2024-01-10 09:40:37 +0100

    second

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-pub trait MeinTrait {
-    fn print(&self, str: String)   { print!("default"); }
+fn kleener<T: PartialOrd>(a: T, b: T) -> T {
+    if a < b { a } else { b }
 }
-
-struct MeinTyp;
-
-impl MeinTrait for MeinTyp {
-    fn print(&self, arg: String) {
-	print!("{arg:}");
-    }
-}
-
 fn main() {
-    let m = MeinTyp;
-    m.print("Hello, world!".to_string());
+    println!("kleener: {}", kleener(2,3));
 }
