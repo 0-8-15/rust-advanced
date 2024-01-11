5874f486d1e0d0165d854f462df1d7eb415a2885
WITTENBERGER Joerg (SAFRAN ENGINEERING SERVICES) <L045738@one.ad>
2024-01-11 14:39:23 +0100

    reqwest get thing

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,29 @@
-// Importieren notwendiger Module und Typen aus der actix_web-Bibliothek.
-use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use actix_web::guard;
-use std::sync::Mutex;
+use std::f64::consts::E;
 
-// This struct represents state
-struct AppStateWithCounter {
-    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+use actix_web::Error;
+use serde;
+#[derive(serde::Deserialize, Debug)]
+struct Pet {
+    id: i32,
+    name: String
 }
 
-// Definiert eine asynchrone Handler-Funktion `hello` für GET-Anfragen an die Wurzelroute ("/").
-#[get("/hihi")]
-async fn hello(data: web::Data<AppStateWithCounter>) -> impl Responder {
-    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    *counter += 1; // <- access counter inside MutexGuard
-
-    format!("Request number: {counter}") // <- response with count
+#[tokio::main]
+async fn main() {
+    let one = get_pet(10).await;
+    println!("Status: {}", one.name);
 }
 
-// Definiert eine asynchrone Handler-Funktion `echo` für POST-Anfragen an die Route "/echo".
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    // Sendet eine HTTP-Antwort mit dem Statuscode 200 (OK) und gibt den Anfrage-Textkörper zurück.
-    HttpResponse::Ok().body(req_body)
-}
-
-// Definiert eine weitere asynchrone Handler-Funktion `manual_hello`.
-async fn manual_hello(req: HttpRequest) -> impl Responder {
-    // Sendet eine HTTP-Antwort mit dem Statuscode 200 (OK) und dem Textkörper "Hey there!".
-    let id = req.match_info().query("you");
-    HttpResponse::Ok().body(format!("Hey {id:}!"))
-}
-
-// Markiert die Hauptfunktion als asynchronen Einstiegspunkt für Actix Web.
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // Note: web::Data created _outside_ HttpServer::new closure
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
-    // Erstellt und startet einen neuen HTTP-Server.
-    // !!! Use paperclip instead.
-    HttpServer::new(move || {
-        // Erstellt eine neue Actix-App.
-        App::new()
-            .app_data(counter.clone()) // <- register the created data
-            // Fügt den `hello`-Handler als Service für GET-Anfragen hinzu.
-	    .service(
-                web::scope("/")
-                    .guard(guard::Host("127.0.0.1"))
-                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
-            )            .service(hello)
-            // Fügt den `echo`-Handler als Service für POST-Anfragen hinzu.
-            .service(echo)
-            // Definiert eine Route "/hey" und setzt `manual_hello` als Handler für GET-Anfragen.
-            .route("/hey/{you}", web::get().to(manual_hello))
-    })
-    // Bindet den Server an die Adresse "127.0.0.1" auf Port 8080.
-    .bind(("127.0.0.1", 8080))?
-    // Startet den Server und wartet auf eingehende Verbindungen.
-    .run()
-    // Wartet auf das Ende des Servers.
-    .await
+async fn get_pet(id: u64) -> Pet {
+    let x = match reqwest::get(format!("https://petstore3.swagger.io/api/v3/pet/{id:}")).await {
+        Ok(response) => response,
+        Err(x) => panic!("Nixda")
+    };
+    let y: String = match x.text().await {
+        Ok(text) => text,
+        Err(_) => panic!("Noch schlimmer")
+    };
+    println!("Status: {y:}");
+    let yj = serde_json::from_str::<Pet>(&y).unwrap();
+    yj
 }
