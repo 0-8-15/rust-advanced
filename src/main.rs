be9977e85d58d2e3bf354218492faf9efa943df3
WITTENBERGER Joerg (SAFRAN ENGINEERING SERVICES) <L045738@one.ad>
2024-01-11 16:10:05 +0100

    rusqlite with trait as it should be

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,82 @@
 use rusqlite::{Connection, Result};
 
-#[derive(Debug)]
-struct Person {
+use std::f64::consts::E;
+
+use actix_web::Error;
+use serde;
+#[derive(serde::Deserialize, Debug)]
+struct Pet {
     id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
+    name: String
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() {
+    let one = get_pet(10).await;
+    println!("Status: {}", one.name);
+    do_main();
+}
+
+async fn get_pet(id: u64) -> Pet {
+    let x = match reqwest::get(format!("https://petstore3.swagger.io/api/v3/pet/{id:}")).await {
+        Ok(response) => response,
+        Err(x) => panic!("Nixda")
+    };
+    let y: String = match x.text().await {
+        Ok(text) => text,
+        Err(_) => panic!("Noch schlimmer")
+    };
+    println!("Status: {y:}");
+    let yj = serde_json::from_str::<Pet>(&y).unwrap();
+    yj
+}
+
+
+trait PetShop {
+    fn add_pet(&self, pet: Pet) -> Result<()>;
+}
+
+impl PetShop for Connection {
+    fn add_pet(&self, pet: Pet)  -> Result<()> {
+	match self.execute(
+            "INSERT INTO pet (name, data) VALUES (?1, ?2)",
+            (&pet.id, &pet.name),
+	) {
+	    Ok(_) => Ok(()),
+	    Err(x) => Err(x)
+	}
+    }
+}
+
+fn do_main() -> Result<()> {
     let conn = Connection::open_in_memory()?;
 
     conn.execute(
-        "CREATE TABLE person (
+        "CREATE TABLE pet (
             id    INTEGER PRIMARY KEY,
             name  TEXT NOT NULL,
             data  BLOB
         )",
         (), // empty list of parameters.
     )?;
-    let me = Person {
+    let alex = Pet {
         id: 0,
-        name: "Steven".to_string(),
-        data: None,
+        name: "Alex".to_string(),
+        // data: None,
     };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    )?;
+    &conn.add_pet(alex);
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
+    let mut stmt = conn.prepare("SELECT id, name, data FROM pet")?;
+    let pets = stmt.query_map([], |row| {
+        Ok(Pet {
             id: row.get("id")?,
             name: row.get(1)?,
-            data: row.get(2)?,
+            // data: row.get(2)?,
         })
     })?;
 
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
+    for pet in pets {
+        println!("Found pet {:?}", pet.unwrap());
     }
     Ok(())
 }
