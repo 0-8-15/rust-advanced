f6e7c57acf63f9a8bf6ea8b2f5e9a7dbe006be5f
Jörg F. Wittenberger <u@berfert.speedport.ip>
2024-02-21 20:58:09 +0100

    enable rusqlite feature 'functions' and export a uuid function

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,18 @@
-use rusqlite::{self, Connection};
+use rusqlite::{self, Connection, functions::FunctionFlags};
+use uuid::{Uuid};
 
 use super::{DATABASE_FILE_NAME};
 
 pub fn open_db() -> Result<Connection, rusqlite::Error>{
     let conn = Connection::open(DATABASE_FILE_NAME)?;
+    conn.create_scalar_function(
+        "uuid",
+        0,
+        FunctionFlags::SQLITE_INNOCUOUS,
+        |_ctx| {
+            let value = Uuid::new_v4().to_string();
+            Ok(value)
+        },
+    )?;
     Ok(conn)
 }
